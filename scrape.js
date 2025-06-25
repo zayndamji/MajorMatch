d17b0908d276cd6127b4a6fa7e454bf6a755dd10
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2025-06-25 15:18:47 -0700

    added example.json and adjusted schema with new types

--- scrape.js
+++ scrape.js
@@ -2,9 +2,10 @@ const AI = require('./ai');
 const fs = require('fs');
 
 const schema = fs.readFileSync('schema');
+const example = fs.readFileSync('example.json');
 
 async function scrape() {
-  const content = await AI(`Can you format Stanford's CS BS program (https://bulletin.stanford.edu/programs/CS-BS) using this schema:\n${schema}\n\nOnly give me the JSON formatted output. Do not write \`\`\` before or after the code. Do not write anything additional than what is asked for in the schema.\nMake sure to include ALL required courses using the webpage provided.`);
+  const content = await AI(`Format Stanford's CS BS program (https://bulletin.stanford.edu/programs/CS-BS) using this schema:\n${schema}\nHere is an example output:\n${example}\nOnly give me the JSON formatted output. Do not write \`\`\` before or after the code. Do not write anything additional than what is asked for in the schema.\nDo not include any specific Capstone/Honors/Subplan/etc program in these requirements.\nOnly include ALL required courses for the major itself.`);
   console.log(content);
   fs.writeFileSync('data/stanford-cs-bs.json', content);
 }
