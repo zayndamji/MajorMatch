af908d91ec8e85968825256e0e9806a88261f48d
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2025-06-26 00:52:07 -0700

    added a catchall

---
+++ src/routes/[...catchall]/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { page } from '$app/stores';
+</script>
+
+<section class="section has-background-dark is-fullheight">
+  <div class="container has-text-centered">
+    <h1 class="title has-text-white">404 - Page Not Found</h1>
+    <p class="subtitle has-text-grey-light">
+      The page <strong class="has-text-white">{ $page.url.pathname }</strong> does not exist.
+    </p>
+    <a href="/" class="button is-outlined mt-4">Go Home</a>
+  </div>
+</section>
+
+<style>
+  :global(html), :global(body) {
+    height: 100%;
+    margin: 0;
+    background-color: #363636; /* Bulma has-background-dark */
+    color: white;
+  }
+</style>
