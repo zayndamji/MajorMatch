52ed85f0dde6cc6fadbcb7f8d2b9ec511fa85f48
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2025-06-26 00:56:41 -0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+export default {
+  kit: {
+    adapter: adapter({
+      // default options are usually fine
+      pages: 'build',     // output directory for static files
+      assets: 'build',    // assets directory
+      fallback: null,     // if using SPA fallback, set to 'index.html'
+      precompress: false, // optional gzip brotli compression
+    })
+  }
 };
-
-export default config;
