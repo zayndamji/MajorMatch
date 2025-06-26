93f8f53ca0e76beac15567cd6f0f41049816f913
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2025-06-26 13:39:06 -0700

    fixed redirect

---
+++ src/routes/stanford/majors/+page.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+  <meta http-equiv="refresh" content="0; URL=/stanford" />
+</svelte:head>
+
+<section class="section has-background-dark is-fullheight">
+  <div class="container has-text-centered">
+    <h1 class="title has-text-white">Redirecting...</h1>
+    <p class="has-text-grey-light">
+      You are being redirected to <a href="/stanford" class="has-text-link">/stanford</a>.
+    </p>
+  </div>
+</section>
+
+<style>
+  a {
+    text-decoration: underline;
+  }
+</style>
