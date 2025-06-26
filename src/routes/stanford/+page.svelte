70343a2d850698689b1b5bdb5f9419c382157c90
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2025-06-26 12:19:23 -0700

    removed unneccesary data from stanford majors and added more styling

--- src/routes/stanford/+page.svelte
+++ src/routes/stanford/+page.svelte
@@ -3,20 +3,26 @@
 </script>
 
 <section class="section has-background-dark is-fullheight">
-  <div class="container">
-    <h1 class="title has-text-centered has-text-white">Stanford University</h1>
+  <div class="container has-text-centered">
+    <!-- Stanford Icon -->
+    <img src="/images/stanford.png" alt="Stanford University logo" class="stanford-icon mb-2" />
+
+    <h1 class="title has-text-white mb-4">Stanford University</h1>
 
     <div class="is-flex is-flex-direction-column is-align-items-center mt-5">
-      {#each majors as major (major.id)}
+      {#each majors as major (major.shortName)}
         <a
           class="box major-box has-text-white has-text-left mb-4 p-4"
-          href={`/stanford/majors/${major.name}`}
+          href={`/stanford/majors/${major.shortName}`}
         >
-          <h2 class="title is-5 has-text-white">
-            {major.longName} – {major.type}
-          </h2>
-          <p class="is-size-7 has-text-grey-light">
-            {major.catalogDescription}
+          <p class="has-text-weight-bold is-size-4 has-text-white">
+            {major.name}
+          </p>
+          <p class="is-size-5 has-text-white mb-1">
+            {major.shortName}
+          </p>
+          <p class="is-size-6 has-text-grey-light">
+            {major.description}
           </p>
         </a>
       {/each}
@@ -25,8 +31,14 @@
 </section>
 
 <style>
+  .stanford-icon {
+    width: 80px;
+    height: auto;
+    display: inline-block;
+  }
+
   .major-box {
-    width: 90%;
+    width: 100%;
     background-color: #1a1a1a;
     border: 1px solid #333;
     transition: background-color 0.2s ease;
