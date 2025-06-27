d9f801cfff64176e3add4f3c7a457e68ca5343a2
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2025-06-26 21:19:33 -0700

    attempted to parse uc berkeley majors

---
+++ scrape/old/ucberkeley.js
@@ -0,0 +1,40 @@
+import fs from 'fs'
+import xml2js from 'xml2js';
+
+const parser = new xml2js.Parser();
+
+let programs = [];
+
+async function getProgram(id) {
+  const res = await fetch(`https://guide.berkeley.edu/ribbit/index.cgi?page=getprogrampopup.rjs&id=${id}&level=ugrad`);
+  
+  let xml = await res.text();
+  xml = xml.replace('<![CDATA[', '');
+  xml = xml.replace(']]>', '')
+
+  console.log(xml);
+
+  parser.parseString(xml, (err, result) => {
+    if (err) {
+      console.error('Error parsing XML:', err);
+      return;
+    }
+
+    const description = result.programpopupinfo.program[0].div[1].div[0].p[0];
+
+    console.log(description + '\n');
+
+    programs.push({
+      description: description,
+      id: id
+    });
+  });
+}
+
+async function main() {
+  for (let i = 1; i <= 156; i++) {
+    await getProgram(i);
+  }
+
+  fs.writeFileSync('./ucberkeleymajors.json', JSON.stringify(programs, undefined, 2));
+} main();
