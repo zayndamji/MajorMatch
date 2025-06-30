c531ebfeb393f713c3ad84a1408268eda51e9fc0
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2025-06-30 16:43:32 -0700

    added more content to error and catchall pages

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -4,11 +4,19 @@
 
 <section class="section has-background-dark is-fullheight">
   <div class="container has-text-centered">
-    <h1 class="title has-text-white">404 - Page Not Found</h1>
+    <h1 class="title has-text-white mb-6">404 - Page Not Found</h1>
+
     <p class="subtitle has-text-grey-light">
       The page <strong class="has-text-white">{ $page.url.pathname }</strong> does not exist.
+      <br />
+      Right now, this website is under construction.
+      <br />
+      Only the Home page, university page, Stanford, and UC Berkeley pages have been built.
     </p>
+
     <a href="/" class="button is-outlined mt-4">Go Home</a>
+    <a href="/universities/stanford" class="button is-outlined mt-4">Go to Stanford University</a>
+    <a href="/universities/ucb" class="button is-outlined mt-4">Go to UC Berkeley</a>
   </div>
 </section>
 
