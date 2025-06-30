eaa4dcf80621890cf0991e0b6c17ec050553bd9c
zaydam1000000 <86502397+zayndamji@users.noreply.github.com>
2025-06-29 19:44:56 -0700

    moved to /output

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,8 @@ import adapter from '@sveltejs/adapter-static';
 export default {
   kit: {
     adapter: adapter({
-      pages: 'build',
-      assets: 'build',
+      pages: 'output',
+      assets: 'output',
       fallback: 'error.html',
       precompress: false,
     })
